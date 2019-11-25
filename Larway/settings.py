82cdcdb3e34c544312d8fa10d383346b2cbc19db
sushant021 <nsushant021@gmail.com>
2019-11-25 14:32:14 -0500

    Settings updated

--- Larway/settings.py
+++ Larway/settings.py
@@ -127,9 +127,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = ()
+
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
